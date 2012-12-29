dac91b90dda46a63e2b7404e67051cc72411c076
Tom Xue <tomxue0126@gmail.com>
2012-12-29 22:26:41 +0800

    add new version: apply Runnable

---
+++ threadTrial_Runnable/src/com/example/threadtrial/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.threadtrial;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	private EditText edittext1, edittext2;
+	volatile boolean bThreadRun = false;
+
+	MyThread1 myThread1;
+	MyThread2 myThread2;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		edittext1 = (EditText) findViewById(R.id.EditText1);
+		edittext2 = (EditText) findViewById(R.id.EditText2);
+
+		edittext1.setText("___thread1___\n");
+		edittext2.setText("___thread2___\n");
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		bThreadRun = true;
+		MyThread1 myThread1 = new MyThread1();
+		new Thread(myThread1).start();
+		MyThread2 myThread2 = new MyThread2();
+		new Thread(myThread2).start();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		bThreadRun = false;
+		// myThread.stop();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	private Handler mHandler1 = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case 1:
+				updateEditText1();
+				break;
+			}
+		};
+	};
+
+	private Handler mHandler2 = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case 2:
+				updateEditText2();
+				break;
+			}
+		};
+	};
+
+	public void updateEditText1() {
+		edittext1.append("thread1\n");
+	}
+
+	public void updateEditText2() {
+		edittext2.append("thread2\n");
+	}
+
+	public class MyThread1 implements Runnable {
+		public MyThread1() {
+		}
+
+		@Override
+		public void run() {
+			while (bThreadRun) {
+//				mHandler1.postDelayed(this, 1000);
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				Message message = new Message();
+				message.what = 1;
+				mHandler1.sendMessage(message);				
+			}
+		}
+	}
+
+	public class MyThread2 implements Runnable {
+		public MyThread2() {
+		}
+
+		@Override
+		public void run() {
+			while (bThreadRun) {
+//				mHandler2.postDelayed(this, 1000);
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				Message message = new Message();
+				message.what = 2;
+				mHandler2.sendMessage(message);				
+			}
+		}
+	}
+}
